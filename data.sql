818f5c766558371bb4c9b0f6eae9cbac6a11540f
MELB26 <90427804+MELB26@users.noreply.github.com>
2024-12-08 21:58:14 +0800

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_accounts (
+CREATE TABLE user_account (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     first_name VARCHAR(50) NOT NULL,
